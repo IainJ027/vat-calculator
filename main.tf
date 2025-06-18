7950b619e62f935a03cf67749e630311f0f49609
IainJ027 <144474118+IainJ027@users.noreply.github.com>
2025-06-18 16:19:29 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "docker_server" {
   ami = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
   instance_type = "t3.medium"
   subnet_id = "subnet-032dac2b9ddd88ce9"
-  vpc_security_group_ids = ["qls-209819-0578d2a2749a6fa1-LabSecGroup-7bLjlnIL5jMi"]
+  vpc_security_group_ids = ["sg-0dafcaf55171837b9"]
   tags = {
     Name = "DockerServer"
   }
